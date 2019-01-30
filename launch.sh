31c4beb3f093c5a6cfecdcdbd2a4e0ecf20973f4
Yeung Yiu Hung <hkclex@gmail.com>
2019-01-30 23:27:01 +0800

    Update lauch.sh

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ echo 127.0.0.1 localhost > /etc/hosts
 # "/var/tmp/nginx" owned by "nginx" user is unusable on heroku dyno so re-create on runtime
 mkdir /var/tmp/nginx
 
-envsubst \$PORT < /nginx-template/taiko-web.template > /etc/nginx/sites-enabled/taiko-web
+envsubst \$PORT < /nginx-template/taiko-web.template > /etc/nginx/conf.d/*.conf
 sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
 supervisord -n
 
