b51cb1fc8c077d8e102e4e06d0aacc6bb42fb54f
Yeung Yiu Hung <hkclex@gmail.com>
2019-01-30 23:16:11 +0800

    Add tmp file

--- launch.sh
+++ launch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo 127.0.0.1 localhost > /etc/hosts
+
+# "/var/tmp/nginx" owned by "nginx" user is unusable on heroku dyno so re-create on runtime
+mkdir /var/tmp/nginx
+
 envsubst \$PORT < /nginx-template/taiko-web.template > /etc/nginx/sites-enabled/taiko-web
+sed -i -E "s/^user .*/user $(whoami);/" /etc/nginx/nginx.conf
 supervisord -n
 
